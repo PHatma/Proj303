56ff15ef7d29238289fbbfc33978fa16ba824282
Rehab200 <201927029@std.sci.cu.edu.eg>
2022-05-01 11:39:49 +0200

    navigation

--- Elegance/App.js
+++ Elegance/App.js
@@ -18,3 +18,53 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+// import {NavigationContainer} from '@react-navigation/native';
+// import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+// const Stack = createNativeStackNavigator();
+// const Tab = createBottomTabNavigator();
+
+// import Home from "./pages/Home";
+// import Profile from "./pages/Profile";
+// import Payment from "./pages/Payment";
+// import StoredItems from "./pages/StoredItems";
+
+// //import SignIn from "./pages/SignIn";
+// //import SignUp from "./pages/SignUp";
+
+// // function Menu() {
+// //     return (
+// //         <Tab.Navigator>
+// //             <Tab.Screen name="home" component={home}/>
+// //             <Tab.Screen name="StoredItems" component={StoredItems}/>
+// //             <Tab.Screen name="Profile" component={Profile}/>
+// //         </Tab.Navigator>
+// //     );
+// // }
+
+
+// export default function App() {
+//     return (
+//         <NavigationContainer  >
+//             <Tab.Navigator >
+//             <Tab.Screen name="Home" component={Home} />
+//              <Tab.Screen name="Payment" component={Payment} />
+//              <Tab.Screen name="StoredItems" component={StoredItems}/>
+//              <Tab.Screen name="Profile" component={Profile}/>
+//              </Tab.Navigator>
+
+
+//              {/* { <Stack.Navigator initialRouteName="SignIn">
+//                 <Stack.Screen name="Menu" component={Menu}/>
+//               <Stack.Screen name="SignIn" component={SignIn}/>
+//                <Stack.Screen name="SignUp" component={SignUp}/>
+//             </Stack.Navigator> }  */}
+          
+//         </NavigationContainer>
+//     );
+// }
+// const styles = StyleSheet.create({
+    
+//  });
+ 
---
+++ Elegance/pages/StoredItems.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function StoredItems({navigation}){
+  return (
+    <View>
+      <Text>StoredItems</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({})
