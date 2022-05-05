7d2c8cbc85801d2f214e4d80b08b34ae5955646d
PHatma <faaatmahaaani@gmail.com>
2022-05-05 21:05:38 +0200

    Trying

---
+++ Elegance/pages/IntroduceApp.js
@@ -0,0 +1,18 @@
+// import {StatusBar} from 'expo-status-bar';
+// import {Button, ScrollView, StyleSheet, Text, View,Image} from 'react-native';
+
+// export default function Home1({ navigation }) {
+//     return ( 
+//     <View class="navbar">
+//     <View class="container">
+//      <Image src="hanger%20(1).png"/> The fashion 
+//       {/* <ul>
+//       <li><a href="fashion_house.html">Home</a> 
+//       <li><a href="fashion_house2.html">Collection</a>   
+//       <li><a href="#aboutus">About us</a>
+//       <li><a href="#contact">Contact us</a>  
+//      </ul>   */}
+//     </View>  
+//    </View>
+//     );
+// }
