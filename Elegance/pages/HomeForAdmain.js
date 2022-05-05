b67fe1fda39a55b7647b4722821df535533adc28
Rehab200 <201927029@std.sci.cu.edu.eg>
2022-05-05 19:14:12 +0200

    navigation

---
+++ Elegance/pages/HomeForAdmain.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const HomeForAdmain = () => {
+  return (
+    <View>
+      <Text>HomeForAdmain</Text>
+    </View>
+  )
+}
+
+export default HomeForAdmain
+
+const styles = StyleSheet.create({})
